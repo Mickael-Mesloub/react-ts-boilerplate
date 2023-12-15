74a508bd875cf659187073952fb40d08222fde8c
Mickaël Mesloub <mickael.mesloub@gmail.com>
2023-12-15 23:35:41 +0100

    chore: edit eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,13 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
@@ -10,6 +17,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', 'prettier'],
   rules: {
+    'import/no-unresolved': ['error', { commonjs: true, amd: true }],
     'prettier/prettier': 'error',
     'react-refresh/only-export-components': [
       'warn',
