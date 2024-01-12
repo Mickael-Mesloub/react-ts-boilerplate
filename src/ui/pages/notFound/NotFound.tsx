f970f00afd5fb15d978207aa3723ba88196941cd
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-01-12 22:44:30 +0100

    feat(router): add NotFound page

---
+++ src/ui/pages/notFound/NotFound.tsx
@@ -0,0 +1,13 @@
+import './NotFound-styles.scss';
+
+interface NotFoundProps {}
+
+const NotFound = ({}: NotFoundProps) => {
+  return (
+    <div>
+      <p>NotFound</p>
+    </div>
+  );
+};
+
+export default NotFound;
